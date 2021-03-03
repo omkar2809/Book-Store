d1547bd63e89545792c7d644bd35f2060dd6abdf
omkar2809 <zemseom10@gmail.com>
2021-03-03 15:28:48 +0530

    get data

---
+++ lambdas/controllers/shop.js
@@ -0,0 +1,38 @@
+const AWS = require('aws-sdk')
+const uuid = require('uuid')
+
+const { response } = require('../utils/response')
+
+const db = new AWS.DynamoDB.DocumentClient()
+
+const productTable = "products"
+
+exports.getBooks = async event => {
+    try{
+        const { Items: books } = await db.scan({ TableName: productTable }).promise()
+        return response(200, books)
+    }
+    catch(err) {
+        return response(500, {err})
+    }
+}
+
+exports.getBook = async event => {
+    try{
+        const id = event.pathParameters.id;
+        const params = {
+            Key: {
+                id: id
+            },
+            TableName: productTable
+        }
+        const { Item: book } = await db.get(params).promise() 
+        if(!book) {
+            return response(404, {message: 'Book Not Found'})
+        }
+        return response(200, book)
+    }
+    catch(err) {
+        return response(500, {err})
+    }
+}
---
+++ lambdas/utils/response.js
@@ -0,0 +1,49 @@
+// exports.response = {
+//     _200(data = {}) {
+        // return {
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         'Access-Control-Allow-Methods': '*',
+        //         'Access-Control-Allow-Origin': '*',
+        //     },
+        //     statusCode: 200,
+        //     body: JSON.stringify(data),
+        // };
+//     },
+
+//     _400(data = {}) {
+//         return {
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 'Access-Control-Allow-Methods': '*',
+//                 'Access-Control-Allow-Origin': '*',
+//             },
+//             statusCode: 400,
+//             body: JSON.stringify(data),
+//         };
+//     },
+
+//     _500(data = {}) {
+//         return {
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 'Access-Control-Allow-Methods': '*',
+//                 'Access-Control-Allow-Origin': '*',
+//             },
+//             statusCode: 400,
+//             body: JSON.stringify({...data, message: "Internal Server Error"}),
+//         };
+//     },
+// }
+
+exports.response = (code, data) => {
+    return {
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Methods': '*',
+                'Access-Control-Allow-Origin': '*',
+            },
+            statusCode: code,
+            body: JSON.stringify(data),
+        };
+}
